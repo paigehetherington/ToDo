55908f426b4a29ac3993dee001a61368a00258f1
Paige Hetherington <herbmama@gmail.com>
2016-02-08 11:49:47 -0500

    Initial import

---
+++ src/ToDoItem.java
@@ -0,0 +1,12 @@
+/**
+ * Created by vajrayogini on 2/8/16.
+ */
+public class ToDoItem {
+    public String text;
+    public boolean isDone;
+
+    public ToDoItem(String text, boolean isDone) {//generate constructor, select both (hold shift)
+        this.text = text;
+        this.isDone = isDone;
+    }
+}
